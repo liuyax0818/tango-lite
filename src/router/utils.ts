44b8145df1517d55d2ad1eff8313e6e220e83de6
hikari <1803703552@qq.com>
2025-11-06 21:56:07 +0800

    perf: 优化路由基础路径配置

--- src/router/utils.ts
+++ src/router/utils.ts
@@ -1,8 +1,10 @@
 import type { RouterHistory } from 'vue-router'
 import { createWebHashHistory, createWebHistory } from 'vue-router'
 
+const baseUrl = import.meta.env.BASE_URL
+
 function getRouterMode(mode: 'hash' | 'history'): RouterHistory {
-  return mode === 'hash' ? createWebHashHistory('') : createWebHistory('')
+  return mode === 'hash' ? createWebHashHistory(baseUrl) : createWebHistory(baseUrl)
 }
 
 export {
